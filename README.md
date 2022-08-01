bc9cf0f56f178985aab86a0691b6223196e83d30
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-07-31 17:03:29 -0700

    Migrate from jay3332/ril

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ril-py
+
+Rust Imaging Library's Python binding: A high-level image processing crate for Python written in Rust
--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="ril",
+    author="Cryptex",
+    license="MIT",
     version="0.1.0",
+    url="https://github.com/Cryptex-github/ril-py",
+    python_requires=">=3.7",
+    description="Rust Imaging Library's Python binding: A high-level image processing crate for Python written in Rust",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     rust_extensions=[RustExtension("ril.ril", binding=Binding.PyO3)],
     packages=["ril"],
     # rust extensions are not zip safe, just like C-extensions.
