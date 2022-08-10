5b650754c4d910ed9ac59007082ed0e049cb3dc1
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-08-09 21:25:50 -0700

    Bump version in other place

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ copyright = '2022, Cryptex'
 author = 'Cryptex'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
