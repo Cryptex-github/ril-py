b989d2ca9e6b6796b3e3be008fbbc0b60b077ca4
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2022-08-17 11:53:28 -0700

    Add current commit hash

--- .github/workflows/scripts/site_template.py
+++ .github/workflows/scripts/site_template.py
@@ -1,7 +1,8 @@
 import os
+import subprocess
 
 
-TEMPLATE: str = '            <a href="wheels/{0}" class="link"><span style="font-size: x-large;">{0}</span></a>'
+TEMPLATE: str = '            <a href="wheels/{0}" class="link" onmouseover="style=\'text-decoration:underline\'" onmouseout="style=\'text-decoration:none\'"><span style="font-size: x-large;">{0}</span></a>'
 
 def main() -> None:
     with open('site/index.html', 'r') as f:
@@ -9,8 +10,10 @@ def main() -> None:
     
     with open('site/index.html', 'w') as f:
         wheels = [TEMPLATE.format(wheel) for wheel in os.listdir('site/wheels')]
+        long_hash = subprocess.run(["git", "rev-parse", "HEAD"]).stdout.decode('utf-8')
+        short_hash = subprocess.run(["git", "rev-parse", "--short", "HEAD"]).stdout.decode('utf-8')
 
-        f.write(current.replace('<!-- TO REPLACE -->', '\n'.join(wheels)))
+        f.write(current.replace('<!-- TO REPLACE -->', '\n'.join(wheels)).replace('short_hash', short_hash).replace('long_hash', long_hash))
 
 if __name__ == '__main__':
     main()
